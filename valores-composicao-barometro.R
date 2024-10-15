9f3e3a0fa58b6a9e742fb212cdda41a867e4ebf7
Pedro R. Andrade <pedro.andrade@inpe.br>
2024-10-15 11:51:49 -0300

    Documentacao e padronizacao do codigo

--- valores-composicao-barometro.R
+++ valores-composicao-barometro.R
@@ -1,8 +1,14 @@
+# script para gerar as proporcionalidades a partir das tabelas
+# de valores e composicao, supondo que as proporcionalidades
+# sao simplesmente as proporcoes dos valores filhos de um 
+# determinado pai. por exemplo, se um pai tem 3 filhos com valores
+# 20, 20 e 10, as proporcionalidades deles serao 0.4, 0.4 e 0.2,
+# respectivamente.
 
 composicao <- read.csv("c:/Users/Usuario/Downloads/BSMoutcorre/composicao.csv", sep=";")
 
 # convert from id to column name
-idtocol = function(id) paste0("X", id, ".2010")
+idtocol <- function(id) paste0("X", id, ".2010")
 
 valores <- read.csv("c:/Users/Usuario/Downloads/BSMoutcorre/valores.csv", sep=";") %>%
   mutate(across(paste0(idtocol(2:62)), stringr::str_replace, "\\,", ".")) %>%
@@ -10,25 +16,26 @@ valores <- read.csv("c:/Users/Usuario/Downloads/BSMoutcorre/valores.csv", sep=";
 
 result <- valores %>% dplyr::select(1)
 
-pais = unique(composicao$codigo_pai)[-1]
+pais <- unique(composicao$codigo_pai)[-1]
 
 for(pai in pais){
   cat(paste0("Processing ", pai, "\n"))
-  filhos = composicao %>%
+
+  filhos <- composicao %>%
     dplyr::filter(codigo_pai == !!pai) %>%
     .$codigo_filho
 
-  vfilhos = valores %>% dplyr::select(idtocol(filhos))
+  vfilhos <- valores %>% dplyr::select(idtocol(filhos))
 
   if(length(filhos) > 1){
-    row_sum = rowSums(vfilhos)
+    row_sum <- rowSums(vfilhos)
   
     vfilhos <- vfilhos %>% 
       mutate_all(~ ./row_sum) %>%
       round(3)
   }
   else
-    vfilhos[, 1] = 1
+    vfilhos[, 1] <- 1
   
   colnames(vfilhos) <- paste(colnames(vfilhos), ".", pai, sep="")
   result <- cbind(result, vfilhos)
@@ -38,19 +45,19 @@ x = names(result)
 
 # pai vai para a primeira linha do csv de saida
 # if vai para a segunda linha do csv de saida
-pai = as.numeric(gsub("X([0-9]+)\\.2010\\.([0-9]+).*$", "\\2", x))
-pai = ifelse(duplicated(pai), NA, pai)
+pai <- as.numeric(gsub("X([0-9]+)\\.2010\\.([0-9]+).*$", "\\2", x))
+pai <- ifelse(duplicated(pai), NA, pai)
 
-nas = is.na(pai)
-pai = paste0(pai, "-2010", sep = "")
+nas <- is.na(pai)
+pai <- paste0(pai, "-2010", sep = "")
 pai[nas] = ""
 
-id = as.numeric(gsub("X([0-9]+).*$", "\\1", x))
+id <- as.numeric(gsub("X([0-9]+).*$", "\\1", x))
 
-id = paste0(id, "-2010", sep = "")
-id[1] = "id"
+id <- paste0(id, "-2010", sep = "")
+id[1] <- "id"
 
-colnames(result) = c("id", idtocol(4:62))
+colnames(result) <- c("id", idtocol(4:62))
 
 result <- mutate(result, across(paste0(idtocol(4:62)), as.character, "")) %>%
   mutate(, across(paste0(idtocol(4:62)), tidyr::replace_na, "DI"))
@@ -60,5 +67,3 @@ result <- rbind(result, pai)
 result <- rbind(tail(result, 2)[2:1, ], head(result, -2))
 
 write.table(result, "proporcionalidades.csv", row.names = FALSE, col.names = FALSE, sep="|")
-
-
